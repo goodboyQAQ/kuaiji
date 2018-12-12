05d9d2f96755c431399cbfb5ea2c196d286c70ff
wangjian <1371171414@qq.com>
2018-12-12 10:58:19 +0800

    .

--- src/base/Top.vue
+++ src/base/Top.vue
@@ -1,18 +1,34 @@
 <template>
-    <div style="background: yellowgreen">
-      登陆 注册
+    <div style="background:white;height:60px;line-height: 60px;text-align: left">
+      <i class="iconfont icon-caidan" style="font-size:30px;" v-if="open" @click="()=>{this.$emit('update:open',!open)}"></i>
+      <i class="iconfont icon-caidan-copy" style="font-size:30px;" v-else @click="()=>{this.$emit('update:open',!open)}"></i>
+      <span style="vertical-align: super">{{this.$route.meta.title}}</span>
+
     </div>
 </template>
 <script>
     export default {
         data() {
-            return {}
+            return {
+              open:true
+            }
         },
-        methods: {},
+        props:["open"],
+        methods: {
+          a(){
+            console.log("aa");
+          },
+          b(){
+
+          }
+        },
         computed: {},
         components: {},
     }
 </script>
 <style scoped>
-
+  i{
+    margin-left:20px;
+    margin-right:30px;
+  }
 </style>
